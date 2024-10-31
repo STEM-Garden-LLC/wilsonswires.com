499e90810055aafdfa1f6f6926ba0a3a9676a35b
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-10-30 20:45:00 -0600

    Add phone number [ci skip]

--- components/header/Logo.tsx
+++ components/header/Logo.tsx
@@ -1,15 +1,32 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Logo() {
     return (
-        <Link aria-label="Home" href="/">
-            <Image
-                src="/logo/logo_medium_transparent_bg.png"
-                alt="Logo"
-                width={200}
-                height={200}
-            />
-        </Link>
+        <div className={cn("flex", "gap-8", "items-center", "text-3xl")}>
+            <a
+                className={cn(
+                    "hidden",
+                    "invisible",
+                    "lg:inline-block",
+                    "lg:visible",
+                    "text-logo-yellow-light",
+                    "whitespace-nowrap"
+                )}
+                href="tel:111-111-1111"
+            >
+                (111)-111-1111
+            </a>
+            <Link aria-label="Home" href="/">
+                <Image
+                    src="/logo/logo_medium_transparent_bg.png"
+                    alt="Logo"
+                    width={300}
+                    height={300}
+                    className={cn("md:min-w-[200px]", "lg:min-w-[300px]")}
+                />
+            </Link>
+        </div>
     );
 }
